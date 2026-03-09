c357dc0350e658fc52766d83327486e316af9b65
Gabriele <154472848+CetriGabrio@users.noreply.github.com>
2026-03-09 11:06:14 -0400

    Mouse movement, player movement, and button

---
+++ Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,43 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControllerInput : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public Vector2 movement;
+    public AudioSource SFX;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += (Vector3)movement * speed * Time.deltaTime;
+        transform.position = movement;
+    }
+
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        movement = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        Debug.Log("Attack" + context.phase);
+        if(context.performed == true)
+        {
+            SFX.Play();
+        }
+    }
+}
